999f7b46cfa5a0293c6556366114a5a850a70903
liucheng <liucheng@ingram-net.com>
2022-08-11 15:40:32 +0800

    cz89_yc22092

---
+++ dlt_500/cz89/筛选记录.py
@@ -0,0 +1,8 @@
+file = "./data/yc22091_数据2022-08-11.txt"
+with open(file, 'r') as f:
+    content = f.readlines()
+
+fileInput = open('./right_data_092.txt', "a")
+for i in content:
+    if i.find("当前访问") >= 0 or i.find("==") >= 0 or (i.find("08") >= 0 and i.find("18") >= 0 and i.find("20") >= 0 and i.find("22") >= 0 and i.find("24") >= 0 and i.find("28") >= 0):
+        fileInput.write(f"{i.strip()}\n")
